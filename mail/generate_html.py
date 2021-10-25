bb774da4672580ddc6868dc6e62079bbb2d9b9c6
Rohit <23212465+Rohit-7@users.noreply.github.com>
2021-10-26 04:57:45 +0530

    Update generate_html.py

--- mail/generate_html.py
+++ mail/generate_html.py
@@ -24,24 +24,18 @@ for comp in servers:
             response = None
             servers[comp][env] = 502
             print(url,'->','Bad URL.')
+            
 
 table_style = 'border-collapse: collapse;border-radius: 0.5em;overflow: hidden;'
-
 th_style = 'background-color: #111;background-clip: padding-box;color:white;position: sticky;margin-top:1em;top:0;padding: 0.75em 1em; border-bottom: 3px solid white;'
-
 td_style = 'padding: 0.5em 1em; border-bottom: 3px solid white;'
-
 td_style_border = 'border-left: 3px solid white;'
-
 box_style = 'display: inline-block;padding: 0.5em;margin:5px;border-radius: 0.25em;'
-
 green_box = 'background-color: #7CFC00;'
-
 red_box = 'background-color: #FF3131;color:white;'
-
 light_row = 'background-color: #e7e7e7;'
-
 dark_row = 'background-color: #c7c7c7;'
+
 
 html =  ''' <div style='width:auto; max-width:95%; margin:25px auto;'>
 <table style="'''+ table_style +'''">
